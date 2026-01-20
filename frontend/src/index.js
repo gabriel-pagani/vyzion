5d0ac687aca9b2aa509618e99d0b032e30f09399
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-20 07:37:55 -0300

    Remove Font Awesome script from index.html and import fontawesome.css in index.js for better asset management

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
 import "./styles/reset.css";
 import "./styles/index.css";
+import "./styles/fontawesome.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
